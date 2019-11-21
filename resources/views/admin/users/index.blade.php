26079f835b9035c57d0b635d683e712fed990291
Adebola <adebola@Adebolas-MacBook-Pro.local>
2019-11-21 22:08:10 +0100

    admin users index revamped

--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -8,9 +8,33 @@
                     <div class="card-header">Users</div>
 
                     <div class="card-body">
-                        @foreach($users as $user)
-                            {{ $user->name }} - {{ $user->email }}
-                        @endforeach
+                        <table class="table">
+                            <thead>
+                            <tr>
+                                <th scope="col">#</th>
+                                <th scope="col">Name</th>
+                                <th scope="col">Email</th>
+                                <th scope="col">Actions</th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                                @foreach($users as $user)
+                                    <tr>
+                                        <th scope="row">{{ $user->id }}</th>
+                                        <td>{{ $user->name }}</td>
+                                        <td>{{ $user->email }}</td>
+                                        <td>
+                                            <a href="{{ route('admin.users.edit', $user->id) }}">
+                                                <button type="button" class="btn btn-primary btn-sm">Edit</button>
+                                            </a>
+                                            <a href="{{ route('admin.users.destroy', $user->id) }}">
+                                                <button type="button" class="btn btn-danger btn-sm">Delete</button>
+                                            </a>
+                                        </td>
+                                    </tr>
+                                @endforeach
+                            </tbody>
+                        </table>
                     </div>
                 </div>
             </div>
